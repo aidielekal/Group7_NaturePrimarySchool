6e6ba74986e9842a1087cee46a6ba12c3a61ed0e
aidielekal <aidielhaiqalh@gmail.com>
2021-01-21 16:28:08 +0800

    minor semicolon

--- connection.php
+++ connection.php
@@ -1,7 +1,7 @@
 <?php
 
 	$con = mysqli_connect('j5zntocs2dn6c3fj.chr7pe7iynqr.eu-west-1.rds.amazonaws.com',
-							'gvbznkzelw1b12sp','tbd69uccbxsr67y4','mgh2o8syxj258uht')
+							'gvbznkzelw1b12sp','tbd69uccbxsr67y4','mgh2o8syxj258uht');
 							// (hostname, 
 							//	username, pasword, database)
 
