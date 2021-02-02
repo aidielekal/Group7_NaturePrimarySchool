f0984e935af288edec1475119b24aeadf9c269e1
ShahiraZ <shahirazulfa99@gmail.com>
2021-02-03 02:09:25 +0800

    Update editTeachers.php

--- editTeachers.php
+++ editTeachers.php
@@ -1,9 +1,10 @@
 <?php
 		require_once 'connection.php';
-
-		$sel_query = "UPDATE user SET userID='$_POST[usID]',Name ='$_POST[usName]',Username = '$_POST[usUsrName]' WHERE userID='$_POST[usID]'";
+		$query = "UPDATE user SET userID='$_POST[usID]',
+									Name ='$_POST[usName]',
+									Username = '$_POST[usUsrName]' WHERE userID='$_POST[usID]'";
 		 //Execute Query
-		 $result = mysqli_query($con,$sel_query);
+		 $result = mysqli_query($con,$query);
 		 if($result){
 			echo '<script type="text/javascript">';
 			echo ' alert("User details has been updated!\nPlease wait until you are redirected back")';
